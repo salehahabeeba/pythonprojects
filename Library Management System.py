d57acfd0d28d6b399726a45d8e9f2000df45a4ce
salehahabeeba <salehahabeeba19@gmail.com>
2024-11-24 23:54:50 +0530

    changes made

--- Library Management System.py
+++ Library Management System.py
@@ -11,33 +11,36 @@ class Library:
             print(books)
 
     def lend_book(self):
-        inp1=input("Enter the name of the book which you want to borrow\n")
+        inp1=input("Enter the name of the book which you want to borrow\n").capitalize()
         if inp1 not in self.books:
             print("The book you mentioned does not exist")
         elif inp1 not in self.lend_record:
             name = input("Enter your name : ")
             self.lend_record[inp1] = name
+            print("The book is lent to you")
         elif inp1 in self.lend_record.keys():
-                print(f"Book is not available in library this book taken by {self.lend_record[inp1]}")
+                print(f"Book is not available in library this book is taken by {self.lend_record[inp1]}")
 
     def add_book(self):
         inp2=input("Enter the name of the book you want to submit in the library\n")
         self.books.append(inp2)
+        print("The book is added to the library")
 
     def return_book(self):
         inp3=input("Enter the name of the book you want to return\n")
         if inp3 in self.lend_record:
             self.lend_record.pop(inp3)
+            print("The book is returned to the library")
         else:
             print("Please enter valid input")
 
-saleha= Library(['Hindi','Telugu','English','Maths','Science','Social','new subject'],"Saleha_Library")
+saleha= Library(['Hindi','Telugu','English','Maths','Science','Social'],"Saleha_Library")
 
 if __name__ == '__main__':
  while True:
       print("---------------------------------------------------------------------------------")
       inp=int(input("Press 1 to know the details of the books\nPress 2 to borrow a book\nPress 3 to add a book\n"
-                  "Press 4 to return a book\nPress 5 for exit"))
+                  "Press 4 to return a book\nPress 5 for exit\n"))
       if inp==1:
         saleha.display()
       elif inp==2:
